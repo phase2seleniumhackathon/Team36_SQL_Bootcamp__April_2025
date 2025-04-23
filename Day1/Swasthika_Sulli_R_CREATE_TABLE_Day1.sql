d634fbb0fad8c1e76aa6def79b6942ee3693edec
dsalgoproject <swasthika.cse@gmail.com>
2025-04-23 10:31:36 -0400

    Commiting Create table SQL Queries and ERD by Swasthika

---
+++ Day1/Swasthika_Sulli_R_CREATE_TABLE_Day1.sql
@@ -0,0 +1,70 @@
+create table categories
+(categoryID int primary key,
+categoryName varchar(255),
+description varchar(255)
+);
+
+select * from categories;
+
+create table customers (customerID
+varchar(255) primary key, companyName
+ varchar(255), 
+ contactName varchar(255) not null, 
+ contactTitle  varchar(255), 
+ city varchar(255), 
+ country varchar(255) not null
+);
+
+select * from customers;
+
+create table employees
+(employeeID int primary key, 
+employeeName varchar(255) not null,
+title varchar(255),
+city varchar(255),
+country varchar(255),
+reportsTo int);
+
+select * from employees;
+
+
+CREATE TABLE shippers(shipperID	int primary key,
+companyName  varchar(255)
+);
+
+select * from shippers;
+
+CREATE TABLE orders(
+orderID INTEGER primary key,
+customerID VARCHAR(255) references customers(customerID),
+employeeID INT references employees(employeeID),
+orderDate Date,
+requiredDate Date,
+shippedDate Date,
+shipperID INT references shippers(shipperID),
+freight FLOAT
+);
+
+select * from orders;
+
+create table products(
+productID int primary key,
+productName varchar(255),
+quantityPerUnit varchar(255),
+unitPrice float,
+discontinued int,	
+categoryID int references categories(categoryID));
+
+select * from products;
+
+create table order_details(orderID int references orders(orderID),
+	productID int references products(productID),
+	unitprice float,
+	quantity int,
+	discount float,
+	primary key(orderID,productID));
+
+
+select * from order_details;
+	
+
