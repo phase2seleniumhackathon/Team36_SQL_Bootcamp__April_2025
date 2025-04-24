a02795a069afe18cbe2ea7d139dd85dfef4f426c
Shobha2013 <rani.shoba94@gmail.com>
2025-04-24 12:08:33 -0500

    Create Querying.sql

---
+++ Day2/Shobha/Querying.sql
@@ -0,0 +1,15 @@
+select * from employees
+
+select employeename,title
+from employees;
+
+select * from products
+
+select distinct unitprice
+from products;
+
+select * from customers
+order by companyname;
+
+select productname,unitprice AS price_in_usd
+from products;
