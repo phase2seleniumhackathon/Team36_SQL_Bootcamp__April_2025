719f2b9e826c29e399fd95407e0059597f645efb
Shobha2013 <rani.shoba94@gmail.com>
2025-04-24 12:12:38 -0500

    Create insert.sql

---
+++ Day2/Shobha/insert.sql
@@ -0,0 +1,16 @@
+-- 8)INSERT into orders table:
+--  Task: Add a new order to the orders table with the following details:
+-- Order ID: 11078
+-- Customer ID: ALFKI
+-- Employee ID: 5
+-- Order Date: 2025-04-23
+-- Required Date: 2025-04-30
+-- Shipped Date: 2025-04-25
+-- shipperID:2
+-- Freight: 45.50
+
+insert into orders(
+orderid, customerid, employeeid, orderdate, requireddate, shippeddate, shipperid, freight)
+values('11078', 'ALFKI', '5', '2025-04-23', '2025-04-30', '2025-04-25', '2', '45.50');
+
+select * from orders
